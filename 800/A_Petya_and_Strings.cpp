5dc5a4157d76aaaad3174a0805c93a0a8ead0f36
Piyush Bafna <130243298+piyushdev04@users.noreply.github.com>
2026-01-09 00:05:51 +0530

    .

---
+++ 800/A_Petya_and_Strings.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	string s1, s2;
+	cin >> s1 >> s2;
+
+	// convert both strings to lowercase
+	for (int i = 0; i < s1.size(); i++) {
+		s1[i] = tolower(s1[i]);
+		s2[i] = tolower(s2[i]);
+	}
+
+	if (s1 < s2) {
+		cout << -1 << "\n";
+	} else if (s1 > s2) {
+		cout << 1 << "\n";
+	} else {
+		cout << 0 << "\n";
+	}
+
+	return 0;
+}
