56b5aa082b0a1df0a57ea7978f2d42ee67e6c8ff
Piyush Bafna <130243298+piyushdev04@users.noreply.github.com>
2026-01-12 23:30:28 +0530

    .

---
+++ 800/A_Gravity_Flip.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int n;
+	cin >> n;
+
+	vector<int> a(n);
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+
+	sort(a.begin(), a.end());
+
+	for (int i = 0; i < n; i++) {
+		cout << a[i];
+		if (i + 1 < n) cout << " ";
+	}
+	cout << "\n";
+
+	return 0;
+}
