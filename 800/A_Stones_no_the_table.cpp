5a80269fc18094a53592ae8b3e17ddbd46bc4d5c
Piyush Bafna <130243298+piyushdev04@users.noreply.github.com>
2026-01-17 22:31:15 +0530

    .

---
+++ 800/A_Stones_no_the_table.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int n;
+	cin >> n;
+
+	string s;
+	cin >> s;
+
+	int count = 0;
+	for (int i = 1; i < n; i++) {
+		if (s[i] == s[i - 1]) {
+			count++;
+		}
+	}
+	cout << count << "\n";
+	return 0;
+}
